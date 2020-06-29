aebd4d103d464f87927ac9e96f8634ad259073af
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-29 11:02:59 -0700

    Added : Class StateCensusAnalyser and CSVStateCensus

--- src/main/java/com/bridgelabz/censusanalyser/controller/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/controller/StateCensusAnalyser.java
@@ -1,7 +1,29 @@
 package com.bridgelabz.censusanalyser.controller;
+import com.bridgelabz.censusanalyser.models.CSVStateCensus;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class StateCensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Indian State Census Analyser Problem");
+    public int loadIndiaCensusData(String csvFilePath) throws Exception {
+        int noOfRecords = 0;
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            System.out.println("test");
+            CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(CSVStateCensus.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+            Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
+            Iterable<CSVStateCensus> csvIterable = () -> censusCSVIterator;
+            noOfRecords = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return noOfRecords;
     }
 }
