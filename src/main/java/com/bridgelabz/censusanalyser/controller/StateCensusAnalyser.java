6c48a49edb256108b2ec9c0a59517be6062c3033
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-29 09:17:42 -0700

    Added : Welcome message

---
+++ src/main/java/com/bridgelabz/censusanalyser/controller/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.censusanalyser.controller;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyser Problem");
+    }
+}
